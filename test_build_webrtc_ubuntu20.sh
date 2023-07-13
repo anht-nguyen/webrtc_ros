88aeea95b110d391b9fde686d2ae730bef363cc5
anht-nguyen <tuananh.nguyen28898@gail.com>
2023-07-13 11:27:01 -0400

    edit .sh file

--- test_build_webrtc_ubuntu20.sh
+++ test_build_webrtc_ubuntu20.sh
@@ -11,7 +11,7 @@ fi
 
 mkdir -p src && cd src
 
-git clone https://github.com/anht-nguyen/webrtc_ros.git
+git clone --single-branch --branch ubuntu20 https://github.com/anht-nguyen/webrtc_ros.git
 git clone --single-branch --branch ros1-develop https://github.com/fkie/async_web_server_cpp.git
 
 sudo apt-get install -y -qq libjsoncpp-dev python3-catkin-tools libgtk-3-dev
