6d3f09ad1dbb11a15e75eb00ce40e4d9fe9bee31
anht-nguyen <tuananh.nguyen28898@gail.com>
2023-07-13 15:23:57 -0400

    edit docker build script

--- dockerfiles_ubuntu20/docker_build_run.sh
+++ dockerfiles_ubuntu20/docker_build_run.sh
@@ -3,6 +3,7 @@ echo "BUILDING DOCKER IMAGE"
 docker build -t webrtc_ros_ubuntu20 . #--no-cache
 
 echo "FINISHED BUILDING DOCKER IMAGE. RUNNING DOCKER CONTAINER"
-docker run -d --name webrtc_ros_ubuntu20  webrtc_ros_ubuntu20
+docker run -d --name webrtc_ros_ubuntu20_1  webrtc_ros_ubuntu20
 
-# docker exec -it webrtc_ros_ubuntu20 bash
+echo "DONE"
+docker exec -it webrtc_ros_ubuntu20_1 bash
